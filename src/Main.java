c70e5fe2ddaf1a07c6e8719abc9989a3de10a0a6
MiroTotev <stelt2000@abv.bg>
2024-04-04 15:31:00 +0300

    new projrct

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] word = {"hi", "Hello", "Hello world", "My name is", " new branch"};
+
+        String[] word1 = new String[args.length];
+        word1 = word;
+        System.out.println("Обърнат масив: " + Arrays.toString(word1));
+
+
+        System.out.println("word : " + word1.length);
+        System.out.println("Name" + Arrays.toString(word));
+        System.out.print(" Name");
+    }
+}
